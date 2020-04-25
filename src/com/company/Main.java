dcfcdea11a317c0128a1db2232feb40d73786648
StanislavMyakishev <masiff-s@live.ru>
2020-04-25 10:21:17 +0300

    Lab4.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,26 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] intArr = {10, 20, 15};
-        Float[] floatArr = new Float[3];
-        float lowBorder = 0;
-        float highBorder = 10;
-        Random random = new Random();
-        for (int i = 0; i < floatArr.length; i++) {
-            floatArr[i] = lowBorder + (highBorder - lowBorder) * random.nextFloat();
+        ItemCatalog itemCatalog = new ItemCatalog();
+        for (int i = 0; i < 10000; i++) {
+            itemCatalog.addItem(new GenericItem());
         }
-        NumberArray<Integer> intArray = new NumberArray<>(intArr);
-        NumberArray<Float> floatArray = new NumberArray<>(floatArr);
-
-        System.out.println(intArray.sum());
-        System.out.println(floatArray.sum());
-
-        String line = "Конфеты ’Маска’;45;120";
-        String[] item_fld;
-        item_fld = line.split(";");
-        System.out.println(Arrays.toString(item_fld));
-        FoodItem foodItem = new FoodItem(item_fld[0], Float.parseFloat(item_fld[1]), Short.parseShort(item_fld[2]));
-        foodItem.printAll();
+        long begin = new Date().getTime();
+        for (int i = 0; i < 100000; i++) {
+            itemCatalog.findItemByID(1001);
+        }
+        long end = new Date().getTime();
+        System.out.println("HASH MAP: " + (end - begin));
+        begin = new Date().getTime();
+        for (int i = 0; i < 100000; i++) {
+            itemCatalog.findItemByIDAL(1001);
+        }
+        end = new Date().getTime();
+        System.out.println("ARRAY LIST: " + (end - begin));
     }
 }
